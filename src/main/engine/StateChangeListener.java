78e322482551b875d883234cdee5caddc4e1ce0e
Maksims <m.orlov10@gmail.com>
2023-12-29 17:00:08 +0100

    add Maven support

--- src/main/engine/StateChangeListener.java
+++
@@ -1,9 +0,0 @@
-package main.engine;
-
-/**
- * Interface to allow state switching in the engine from the states (Observer).
- * @author Maksims Orlovs
- */
-public interface StateChangeListener {
-    void changeState(GameState newState);
-}
