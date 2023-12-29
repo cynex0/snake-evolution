b3ba999ddd451e71394cd8843023c1b9dcdc8b0d
Maksims <m.orlov10@gmail.com>
2023-12-29 21:01:17 +0100

    fix merge conflicts

--- src/main/java/objects/food/FoodType.java
+++
@@ -1,9 +0,0 @@
-package objects.food;
-
-/**
- * Represents all possible food types.
- * @author Fatemeh Akbarifar
- */
-public enum FoodType {
-    DEFAULT, SPEEDFOOD, SLOWFOOD, MINUSFOOD, PLUSFOOD, CONTROLINVERTER
-}
--- src/main/java/utilities/Direction.java
+++
@@ -1,9 +0,0 @@
-package utilities;
-
-/**
- * Describes all possible directions.
- * @author Maksims Orlovs
- */
-public enum Direction {
-    UP, DOWN, LEFT, RIGHT;
-}
